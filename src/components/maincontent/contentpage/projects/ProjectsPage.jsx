97c7d294321ee67141e9d1a7dbf6bd142010b817
Pritam Prasad <pritam.prasd@gmail.com>
2021-06-04 22:21:45 +0530

    base code

---
+++ src/components/maincontent/contentpage/projects/ProjectsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ProjectsPage(props) {
+    return (
+        <div className="projects-page">
+            Projects
+        </div>
+    );
+}
+
+
+
+export default ProjectsPage;
