47e802e7acbbe9b5a2e755c95a92a8e5f01b671f
Pritam Prasad <mailpritamprasad@gmail.com>
2021-08-02 00:44:30 +0530

    temp code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Main from './containers/Main';
 
 function App() {
   return (
     <div className="App">
-      Hello
-    </div>
+      <nav className="navbar"></nav>
+      <main>
+        <Main />
+      </main>
+    </div >
   );
 }
 
---
+++ tech.md
@@ -0,0 +1,15 @@
+## Questions:
+
+1. How to have scrollbar style for all browsers?
+
+```css
+body::-webkit-scrollbar {
+  width: 0.5rem;
+}
+body::-webkit-scrollbar-track {
+  background: #1e1e24;
+}
+body::-webkit-scrollbar-thumb {
+  background: #6649b8;
+}
+```
