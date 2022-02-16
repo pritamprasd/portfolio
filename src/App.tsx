5fe53da94c1676606812fdfcec08cec87ecc4405
Pritam Prasad <5097255+pritamprasd@users.noreply.github.com>
2022-02-17 00:39:17 +0530

    test reoved

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,22 @@ function App() {
 
   return (
     <AppShell
-      // navbarOffsetBreakpoint controls when navbar should no longer be offset with padding-left
       navbarOffsetBreakpoint="sm"
-      // fixed prop on AppShell will be automatically added to Header and Navbar
       fixed
       navbar={
         <Navbar
           padding="md"
-          // Breakpoint at which navbar will be hidden if hidden prop is true
           hiddenBreakpoint="sm"
-          // Hides navbar when viewport size is less than value specified in hiddenBreakpoint
           hidden={!opened}
-          // when viewport size is less than theme.breakpoints.sm navbar width is 100%
-          // viewport size > theme.breakpoints.sm – width is 300px
-          // viewport size > theme.breakpoints.lg – width is 400px
           width={{ sm: 300, lg: 400 }}
         >
+          <Text>Application navbar</Text>
+          <Text>Application navbar</Text>
           <Text>Application navbar</Text>
         </Navbar>
       }
       header={
         <Header height={70} padding="md">
-          {/* Handle other responsive styles with MediaQuery component or createStyles function */}
           <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
             <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
               <Burger
