d7b2594a114cdea3a8ee3072016a6c8353b2d574
Pritam Prasad <pritam.prasd@gmail.com>
2022-02-28 03:00:51 +0530

    select to mantine style

--- src/pages/tools/vs-code/Editor.tsx
+++ src/pages/tools/vs-code/Editor.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { languages, themes } from './utils';
 import * as monaco from 'monaco-editor';
-import { Code, Divider, Grid, Space, Text } from '@mantine/core';
+import { Code, Divider, Grid, Select, Space, Text } from '@mantine/core';
 import FileManager from '../../../components/FileManager';
 import { forceUpdateCodeEditorContent, updateCodeEditorContent, updateEditorLag } from './vsEditorSlice';
 import { useDispatch, useSelector } from 'react-redux';
@@ -59,21 +59,32 @@ export const Editor = () => {
     }
     return (
         <div>
-            <select onClick={onLangChange}>
-                {languages.map(l => <option value={l}>{l}</option>)}
-            </select>
-            <select onClick={onThemeChange}>
-                <option value=''>Update Theme</option>
-                {themes.map(l => <option value={l}>{l}</option>)}
-            </select>
-            <Text>Detected Language:     
-                <Code key={editorInstance?.getModel()?.getLanguageId()} 
-                      style={{backgroundColor: styles.primary_error}}>
-                    {editorInstance?.getModel()?.getLanguageId()}
-                </Code>
-            </Text>
-            <Divider/>
-             <Grid style={{width: '100%', height: '100%'}}>
+            <div style={{display: 'flex', flexDirection: 'row', 
+                        justifyContent: 'flex-start', alignItems: 'center', paddingBottom: '1rem',
+                        paddingRight: '1rem'}}>
+                <Select onClick={onLangChange} label="Editor Language" data={languages.map(l => {
+                    return {
+                        label: l,
+                        value: l
+                    }
+                })} value={editorLang} onChange={e => dispatch(updateEditorLag(e || ''))}
+                />
+                <Select onClick={onThemeChange} label="Editor Theme" data={themes.map(l => {
+                    return {
+                        label: l,
+                        value: l
+                    }
+                })} value={editorLang} onChange={e => setEditorTheme(e || '')}
+                style={{padding: '0.5rem'}}
+                />
+                <Text style={{marginLeft: 'auto', marginRight: '0'}}>Detected Language:     
+                    <Code key={editorInstance?.getModel()?.getLanguageId()} 
+                        style={{backgroundColor: styles.primary_error}}>
+                        {editorInstance?.getModel()?.getLanguageId()}
+                    </Code>
+                </Text>
+            </div>
+            <Grid style={{width: '100%', height: '100%'}}>
                 <Grid.Col sm={8} span={12}>
                     <div className="Editor" ref={divEl} style={{width: '100%', height: '70vh'}}/>
                 </Grid.Col>
