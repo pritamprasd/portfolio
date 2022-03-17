95590ec578679193f1260949abd610180ac15712
Pritam Prasad <pritam.prasd@gmail.com>
2022-03-18 02:11:34 +0530

    typo

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -7,11 +7,9 @@ import { styles } from './storage/data';
 
 function LandingPage() {
     const dispatch = useDispatch();
-    var Coder = require('./assets/pngs/man_working.png');
     return (
         <div style={{display:'flex', flexDirection: 'column', height: '100vh', width: '100%', 
                         alignItems: 'center', justifyContent: 'center', gap: '1rem'}}>
-            {/* <img src={Coder} /> */}
             <h1>Portfolio</h1>
             <p style={{color: styles.primary_error}}>pritam.dev</p>
             <button onClick={() => dispatch(updateVisiblePage('default'))}>Explore</button>
